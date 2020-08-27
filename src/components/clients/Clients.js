7cf2fa93a6e6b5e56fe70dd9e4e87c3d5ecd148a
Carla Ghanem <carlaghanem_29@hotmail.com>
2020-08-27 19:32:30 +0300

    Setting firestore and loading data

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,42 +1,67 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import firestore from "firebase/firestore";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
 
 class Clients extends Component {
-  render() {
-    const clients = [
-      {
-        id: "12345",
-        firstName: "Kevin",
-        lastName: "Johnson",
-        email: "kevin@gmail.com",
-        phone: "55-555-555",
-        balance: "30",
-      },
-      {
-        id: "1655",
-        firstName: "John",
-        lastName: "Doe",
-        email: "jdoe@gmail.com",
-        phone: "55-444-555",
-        balance: "100",
-      },
-    ];
+  state = {
+    totalOwed: null,
+  };
 
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+    if (clients) {
+      //Add balances
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance.toString());
+      }, 0);
+
+      return { totalOwed: total };
+    }
+  }
+
+  render() {
+    // const clients = [
+    //   {
+    //     id: "12345",
+    //     firstName: "Kevin",
+    //     lastName: "Johnson",
+    //     email: "kevin@gmail.com",
+    //     phone: "55-555-555",
+    //     balance: "30",
+    //   },
+    //   {
+    //     id: "1655",
+    //     firstName: "John",
+    //     lastName: "Doe",
+    //     email: "jdoe@gmail.com",
+    //     phone: "55-444-555",
+    //     balance: "100",
+    //   },
+    // ];
+
+    const { clients } = this.props;
+    const { totalOwed } = this.state;
     if (clients) {
       return (
         <div>
           <div className='row'>
-            <div classname='col-md-6'>
+            <div className='col-md-6'>
               <h2>
                 <i className='fas fa-users'></i>Clients
               </h2>
             </div>
-            <div classname='col-md-6'></div>
+            <div className='col-md-6'>
+              <h5 className='text-right text-secondary'>
+                Total Owed{" "}
+                <span className='text-primary'>
+                  ${parseFloat(totalOwed).toFixed(2)}
+                </span>
+              </h5>
+            </div>
           </div>
           <table className='table table-striped'>
             <thead className='thead-inverse'>
@@ -69,20 +94,20 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1>Loading...</h1>;
+      return <Spinner />;
     }
   }
 }
-// Clients.propTypes = {
-//   firestore: PropTypes.object.isRequired,
-//   clients: PropTypes.array,
-// };
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array,
+};
 
-// export default compose(
-//   firestoreConnect(() => ["clients"]), // or { collection: 'todos' }
-//   connect((state, props) => ({
-//     clients: state.firestore.ordered.clients,
-//   }))
-// )(Clients);
+export default compose(
+  firestoreConnect([{ collection: "clients" }]), // or { collection: 'todos' }
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients,
+  }))
+)(Clients);
 
-export default Clients;
+// export default Clients;
--- src/store.js
+++ src/store.js
@@ -1,18 +1,10 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
+import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
 //@todo
-
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore"; // <- needed if using firestore
-import { createStore, combineReducers, compose } from "redux";
-import {
-  ReactReduxFirebaseProvider,
-  firebaseReducer,
-} from "react-redux-firebase";
-import { createFirestoreInstance, firestoreReducer } from "redux-firestore"; // <- needed if using firestore
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGMPqG5cq9qlPYbr4mbpxO1-P0PUHwC-w",
@@ -24,6 +16,11 @@ const firebaseConfig = {
   appId: "1:882292918947:web:65946ded2be3e416914d16",
   measurementId: "G-Z47VGSL4Z3",
 };
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Initialize other services on firebase instance
+const firestore = firebase.firestore(); // <- needed if using firestore
 
 // react-redux-firebase config
 const rrfConfig = {
@@ -31,11 +28,11 @@ const rrfConfig = {
   useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
 };
 
-// Initialize firebase instance
-firebase.initializeApp(firebaseConfig);
-
-// Initialize other services on firebase instance
-firebase.firestore(); // <- needed if using firestore
+//Add react-redux firebase enhancer when making creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
 
 // Add firebase to reducers
 const rootReducer = combineReducers({
@@ -45,14 +42,21 @@ const rootReducer = combineReducers({
 
 // Create store with reducers and initial state
 const initialState = {};
-const store = createStore(rootReducer, initialState);
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance, // <- needed if using firestore
-};
+// const rrfProps = {
+//   firebase,
+//   config: rrfConfig,
+//   dispatch: store.dispatch,
+//   createFirestoreInstance, // <- needed if using firestore
+// };
 //end
 
 export default store;

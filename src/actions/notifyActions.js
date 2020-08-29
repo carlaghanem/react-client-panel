b641aeb08f03d06b53eaa4ae3145987a7be09336
Carla Ghanem <carlaghanem_29@hotmail.com>
2020-08-29 18:40:31 +0300

    Alert

---
+++ src/actions/notifyActions.js
@@ -0,0 +1,5 @@
+import { NOTIFY_USER } from "./Types";
+
+export const notifyUser = (message, messageType) => {
+  return { type: NOTIFY_USER, message, messageType };
+};
--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
-//@todo
+import notifyReducer from "./reducers/notifyReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGMPqG5cq9qlPYbr4mbpxO1-P0PUHwC-w",
@@ -39,7 +39,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer, // <- needed if using firestore
+  firestore: firestoreReducer,
+  notify: notifyReducer,
 });
 
 // Create store with reducers and initial state
